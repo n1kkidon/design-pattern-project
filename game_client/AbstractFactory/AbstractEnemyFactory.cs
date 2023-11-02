5579966fcf6656b5326e52fc812b88d5ea9e40f7
hoolis <rokaskry@gmail.com>
2023-11-02 10:34:47 +0200

    Abstract Factory

---
+++ game_client/AbstractFactory/AbstractEnemyFactory.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Media;
+using shared;
+
+namespace game_client.AbstractFactory
+{
+    public abstract class AbstractEnemyFactory
+    {
+        public abstract IEnemyPixel CreateEnemyPixel(string name, Color color, Vector2 location);
+        public abstract IEnemyStats CreateEnemyStats(string enemyType);
+    }
+}
